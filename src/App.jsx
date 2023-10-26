e049dece26475fb4e8b569779eff0503b8d2c636
Bin Budiman Affandi <b.binbudimanaffandi@dxc.com>
2023-10-26 09:57:19 +0800

    save to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,14 @@ import { nanoid } from "nanoid";
 import { ToastContainer, toast } from 'react-toastify';
 import Items from "./Items";
 
+const setLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items));
+}
+
+const defaultList = JSON.parse(localStorage.getItem('list') || '[]'); 
+
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(defaultList);
   
   const addItem = (itemName) => {
     const newItem = {
@@ -13,13 +19,15 @@ const App = () => {
       completed: false,
       id: nanoid(),
     };
-
-    setItems([...items, newItem]);
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
   }
 
   const removeItem = (itemId) => {
     const newList = items.filter((item) => item.id !== itemId);
     setItems([...newList])
+    setLocalStorage(newList);
   }
 
   return <section className="section-center">
